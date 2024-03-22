1f46476f1b46daacb92e6fd9af0cc87c001d7740
Naïa <naia.perinelle@laposte.net>
2024-03-23 00:20:30 +0100

    add k-mers Frequency encoding

--- notebooks/modules/encoding.py
+++ notebooks/modules/encoding.py
@@ -2,6 +2,7 @@
 ref. Fidle
 """
 import numpy as np
+import itertools
 
 AMINO_ACID_ALPHABET = "ARNDCQEGHILKMFPSTWYVXU"
 
@@ -20,10 +21,32 @@ def one_hot_encode_sequence(sequence: str, encoder: dict[str, np.ndarray], vecto
         sequence = sequence[:vector_size]
     for i, letter in enumerate(sequence):
         encoded[i] = encoder[letter]
-        return encoded
+    return encoded
 
 def one_hot_encoder(x: str, encoder: dict[str, np.ndarray], vector_size: int=1500):
     x_encode = []
     for sequence in x:
         x_encode.append(one_hot_encode_sequence(sequence, encoder, vector_size))
     return np.array(x_encode)
+
+"""
+Kmers Frequency
+"""
+
+def alphabet_kmer(alphabet=AMINO_ACID_ALPHABET, k: int=3):
+    kmers_order = itertools.product(*itertools.repeat(alphabet, k))
+    encoding = {}
+    for kmer in kmers_order:
+        encoding[kmer] = 0
+    return encoding
+
+def kmer_frequency_encode(sequence: str, encoder: dict[str, int], k: int=3):
+    kmers = []
+    for i in range(len(sequence)):
+        kmers.append(sequence[i:i+k+1])
+    for subseq in kmers:
+        encoder[subseq] += 1
+    frequency = []
+    for key in encoder.key():
+        frequency.append(encoder[key])
+    return frequency
