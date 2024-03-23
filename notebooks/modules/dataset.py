f822c9f9f07129380e69396c919d289642e38cd5
Samuel Ortion <samuel+git@ortion.fr>
2024-03-23 09:38:53 +0100

    Propose new encoding classes

--- notebooks/modules/dataset.py
+++ notebooks/modules/dataset.py
@@ -15,9 +15,9 @@ class OneHotProtDataset:
         df_seq[["seq_rcpb", "seq_type"]] = df_seq["seq_id"].str.split('|',  n=1, expand=True)
         ordinal_encoder = OrdinalEncoder()
         enc_seq_type = ordinal_encoder.fit_transform(df_seq[["seq_type"]])
-        encoder = encoding.alphabet_one_hot(alphabet=encoding.AMINO_ACID_ALPHABET)
         seq = df_seq["sequence"]
-        seq_encoded = encoding.one_hot_encoder(seq, encoder, vector_size=vector_size)
+        encoder = encoding.ProteinOneHotEncoder(encoding.AMINO_ACID_ALPHABET)
+        seq_encoded = encoder.encode(seq, vector_size=vector_size)
         X_data = seq_encoded
         y_data = enc_seq_type
         alphabet_size = len(encoding.AMINO_ACID_ALPHABET)
--- notebooks/modules/encoding.py
+++ notebooks/modules/encoding.py
@@ -2,28 +2,56 @@
 ref. Fidle
 """
 import numpy as np
+import pandas as pd
 
 AMINO_ACID_ALPHABET = "ARNDCQEGHILKMFPSTWYVXU"
 
-def alphabet_one_hot(alphabet=AMINO_ACID_ALPHABET):
-    base = np.zeros((len(alphabet)))
-    encoding = {}
-    for index, letter in enumerate(alphabet):
-        code = base.copy()
-        code[index] = 1
-        encoding[letter] = code
-    return encoding
+class ProteinOneHotEncoder:
 
-def one_hot_encode_sequence(sequence: str, encoder: dict[str, np.ndarray], vector_size: int) -> np.ndarray:
-    encoded = np.zeros((vector_size, len(encoder)))
-    if len(sequence) > vector_size:
+    def __init__(self, alphabet=AMINO_ACID_ALPHABET):
+        self.alphabet = alphabet
+        self.encoder = self.alphabet_one_hot()
+
+    def alphabet_one_hot(self):
+        base = np.zeros((len(self.alphabet)))
+        encoding = {}
+        for index, letter in enumerate(self.alphabet):
+            code = base.copy()
+            code[index] = 1
+            encoding[letter] = code
+        return encoding
+
+    def one_hot_encode_sequence(self, sequence: str, vector_size: int) -> np.ndarray:
+        encoded = np.zeros((vector_size, len(self.encoder)))
+        if len(sequence) > vector_size:
+            sequence = sequence[:vector_size]
+        for i, letter in enumerate(sequence):
+            encoded[i] = self.encoder[letter]
+        return encoded
+
+    def encode(self, x: str, vector_size: int=1500):
+        x_encode = []
+        for sequence in x:
+            x_encode.append(self.one_hot_encode_sequence(sequence, vector_size))
+        return np.array(x_encode)
+
+class NLFEncoder:
+    def __init__(self, ):
+        self.nlf = pd.read_csv('../data/NFL.csv', index_col=0)
+        # The letter X will represent an empty position (added in case we might encounter empty spots somewhere)
+        self.nlf['X'] = [0.0] * self.nlf.shape[0]
+        # If we encounter Selenocysteine, we'll treat it as Cysteine
+        self.nlf['U'] = self.nlf['C']
+    
+    def encode(self, sequence: str, vector_size: int=1500) -> np.ndarray:
         sequence = sequence[:vector_size]
-    for i, letter in enumerate(sequence):
-        encoded[i] = encoder[letter]
-    return encoded
+        seq_tensor = np.zeros((vector_size, self.nlf.shape[0]), dtype=np.float16)
+        for i, letter in enumerate(sequence):
+            seq_tensor[i] = self.nlf[letter]
+        return seq_tensor
 
-def one_hot_encoder(x: str, encoder: dict[str, np.ndarray], vector_size: int=1500):
-    x_encode = []
-    for sequence in x:
-        x_encode.append(one_hot_encode_sequence(sequence, encoder, vector_size))
-    return np.array(x_encode)
+    def call(self, x):
+        tensors = []
+        for sequence in x:
+            tensors.append(self.encode(sequence))
+        return np.array(tensors)
